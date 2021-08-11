a84201b31205332dbfbea953dd151dbbdb6b4273
Abdullah UĞUZ <abdullahuguz08@gmail.com>
2021-08-11 15:43:49 +0300

    Generic Examples

---
+++ GenericExamples/MyList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericExamples
+{
+    class MyList<T>
+    {
+        T[] temps;
+
+        public MyList()
+        {
+            temps = new T[0];
+        }
+        public void Add(T temp)
+        {
+            T[] tempArray = temps;
+
+            temps = new T[temps.Length+1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                temps[i] = tempArray[i];
+            }
+            temps[temps.Length - 1] = temp;
+        }
+
+         public void ShowMyList()
+        {
+            foreach (var item in temps)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
---
+++ GenericExamples/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList = new MyList<string>();
+
+            myList.Add("Ali");
+            myList.Add("Kemal");
+            myList.ShowMyList();
+
+            MyList<int> myList1 = new MyList<int>();
+
+            myList1.Add(12);
+            myList1.Add(13);
+            myList1.ShowMyList();
+        }
+    }
+}
